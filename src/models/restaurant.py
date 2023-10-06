57bab5cb18b27fb64dcd1b29bdd26cc81599cc0a
Marcelo Coelho <coelho@Acer-A315-41>
2023-10-06 08:08:16 -0300

    Remove prints de algumas funções de Restauran: - describe_restaurant; - open_restaurant; - close_restaurant.

--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -7,28 +7,42 @@ class Restaurant:
         self.number_served = 0
         self.open = False
 
-    def describe_restaurant(self): #MARCELO
+    def describe_restaurant(self):  # Marcelo
         """Imprima uma descrição simples da instância do restaurante."""
-        print(f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}.")
-        print(f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto.")
 
-    def open_restaurant(self): #MARCELO
+        # print(f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}.")
+        # print(f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto.")
+
+        # Melhoria: Removendo prints de dentro da função e acrescentando returns
+        return [f"Esse restaturante chama {self.cuisine_type} and serve {self.cuisine_type}.",
+                f"Esse restaturante está servindo {self.number_served} consumidores desde que está aberto."]
+
+    def open_restaurant(self):  # Marcelo
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
         if not self.open:
             self.open = False
             self.number_served = -2
-            print(f"{self.restaurant_name} agora está aberto!")
-        else:
-            print(f"{self.restaurant_name} já está aberto!")
 
-    def close_restaurant(self): #MARCELO
+            # Melhoria: Removendo prints de dentro da função e acrescentando returns
+            # print(f"{self.restaurant_name} agora está aberto!")
+            return f"{self.restaurant_name} agora está aberto!"
+        else:
+            # Melhoria: Removendo prints de dentro da função e acrescentando returns
+            # print(f"{self.restaurant_name} já está aberto!")
+            return f"{self.restaurant_name} já está aberto!"
+
+    def close_restaurant(self):  # Marcelo
         """Imprima uma mensagem indicando que o restaurante está fechado para negócios."""
         if self.open:
             self.open = False
             self.number_served = 0
-            print(f"{self.restaurant_name} agora está fechado!")
+            # Melhoria: Removendo prints de dentro da função e acrescentando returns
+            # print(f"{self.restaurant_name} agora está fechado!")
+            return f"{self.restaurant_name} agora está fechado!"
         else:
-            print(f"{self.restaurant_name} já está fechado!")
+            # Melhoria: Removendo prints de dentro da função e acrescentando returns
+            # print(f"{self.restaurant_name} já está fechado!")
+            return f"{self.restaurant_name} já está fechado!"
 
     def set_number_served(self, total_customers): #TALES
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
